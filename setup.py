315bdc0c77af07ba2c84bfb69aa2373af0eda9d4
František Jahoda <frantisek.jahoda@gmail.com>
2012-12-14 00:04:24 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='RelationPy',
+      version='0.1',
+      description='Subset of Underscore library working on iterators',
+      author='Frantisek Jahoda',
+      author_email='frantisek.jahoda@gmail.com',
+      url='https://github.com/jahodfra/relationpy',
+      py_modules=['relation'],
+     )
